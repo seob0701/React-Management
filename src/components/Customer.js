e5b9cc7a3caf8b32d109973e96718e1cc46f5a32
seob717 <69154476+seob717@users.noreply.github.com>
2020-09-13 20:04:46 +0900

    Apply Material UI

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,46 +1,20 @@
 import React, { Component } from "react";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
 
 class Customer extends Component {
   render() {
     return (
-      <>
-        <CustomerProfile
-          image={this.props.image}
-          name={this.props.name}
-          id={this.props.id}
-        />
-        <CustomerInfo
-          birthday={this.props.birthday}
-          gender={this.props.gender}
-          job={this.props.job}
-        />
-      </>
-    );
-  }
-}
-
-class CustomerProfile extends Component {
-  //id name image
-  render() {
-    return (
-      <div>
-        <img src={this.props.image} alt="profile" />
-        <h2>
-          {this.props.name}({this.props.id})
-        </h2>
-      </div>
-    );
-  }
-}
-class CustomerInfo extends Component {
-  // birthday, job, gender
-  render() {
-    return (
-      <div>
-        <p>{this.props.birthday}</p>
-        <p>{this.props.gender}</p>
-        <p>{this.props.job}</p>
-      </div>
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell>
+          <img src={this.props.image} alt="profile" />
+        </TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.birthday}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+      </TableRow>
     );
   }
 }
