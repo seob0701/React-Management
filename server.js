daff58ce8f4e581e56f11f7f235ae61d22688285
seob717 <69154476+seob717@users.noreply.github.com>
2020-09-14 20:17:27 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,33 +7,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+const { config } = require("process");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+
+connection.connect();
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "http://placeimg.com/64/64/1",
-      name: "윤수빈",
-      birthday: "990604",
-      gender: "여자",
-      job: "대학생",
-    },
-    {
-      id: 2,
-      image: "http://placeimg.com/64/64/2",
-      name: "김보라",
-      birthday: "960717",
-      gender: "여자",
-      job: "회사원",
-    },
-    {
-      id: 3,
-      image: "http://placeimg.com/64/64/3",
-      name: "이진아",
-      birthday: "970417",
-      gender: "여자",
-      job: "매니저",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
